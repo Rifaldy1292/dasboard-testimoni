84fcf0d3ab5b797a0913f34d20806adcce3cf8ff
jonfry1175 <jonfrymarbun@gmail.com>
2025-02-08 15:26:08 +0700

    be refacor: websocket

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ const { PORT } = require("./config/config.env");
 const { Machine, MachineLog } = require("./models");
 const path = require('path');
 const { createServer } = require('http')
-const { percentage, totalHour } = require("./utils/countHour")
 const { getRunningTime, getRunningTimeMonth } = require("./utils/getRunningTime");
 const handleWebsocket = require("./websocket/");
 
@@ -28,6 +27,7 @@ const wss = new WebSocket.Server({ server });
 handleWebsocket(wss)
 
 const router = require("./routes");
+const MachineWebsocket = require("./websocket/machine");
 app.use("/api", router);
 
 server.listen(PORT, () => {
@@ -36,36 +36,6 @@ server.listen(PORT, () => {
 
 const mqttClient = mqtt.connect('mqtt://localhost:1883');
 const mqttTopics = ['mc-1/data', 'mc-2/data', 'mc-3/data', 'mc-4/data', 'mc-5/data', 'mc-6/data', 'mc-7/data', 'mc-8/data', 'mc-9/data', 'mc-10/data', 'mc-11/data', 'mc-12/data', 'mc-13/data', 'mc-14/data', 'mc-15/data'];
-const perfectTime = 24 //hour
-
-// runningHour = miliseconds
-function getRunningHours(runningHour) {
-  return percentage(runningHour, perfectTime)
-}
-
-function countDescription(totalRunningHours) {
-  const count = totalHour(totalRunningHours)
-  const hour = count.split('.')[0]
-  const minute = count.split('.')[1]
-  return `${hour} hour ${minute} minute / ${perfectTime} hour`
-}
-
-// day or month, default day
-let type = 'day'
-
-
-wss.on('connection', (ws) => {
-  // console.log('Client connected');
-
-  ws.on('message', (message) => {
-    console.log({ message: JSON.parse(message) })
-    const { type } = JSON.parse(message)
-    if (type === 'month') {
-      ws.send(JSON.stringify({ message: 'aakkmkmk' }))
-    }
-  })
-});
-
 
 mqttClient.on('connect', async () => {
   console.log('MQTT client connected');
@@ -111,32 +81,13 @@ mqttClient.on('message', async (topic, message) => {
 
 
   } catch (error) {
-    console.error(error)
+    console.error({ error, message: error.message })
   }
 
   wss.clients.forEach(async (client) => {
     if (client.readyState === WebSocket.OPEN) {
-      const machines = await Machine.findAll({
-        attributes: ['name', 'status', 'total_running_hours']
-      });
-      const formattedMessage =
-        machines.map(machine => {
-          const runningTime = getRunningHours(machine.total_running_hours)
-          // const running = Math.floor(Math.random() * 100);
-          return {
-            name: machine.name,
-            status: machine.status,
-            // percentage: [runningTime, 100 - runningTime],/
-            percentage: [runningTime, 100 - runningTime],
-            description: countDescription(machine.total_running_hours),
-          }
-        }).sort((a, b) => {
-          const numberA = parseInt(a.name.slice(3))
-          const numberB = parseInt(b.name.slice(3))
-          return numberA - numberB
-        })
-
-      client.send(JSON.stringify({ type: 'percentage', data: formattedMessage }));
+      MachineWebsocket.timelines(client)
+      MachineWebsocket.percentages(client)
     }
   });
   console.timeEnd('Proses');
