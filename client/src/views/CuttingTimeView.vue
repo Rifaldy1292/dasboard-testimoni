26cd17cefae6febab5ff04b9dbd972746fb4387a
jonfry1175 <jonfrymarbun@gmail.com>
2025-02-10 10:05:06 +0700

    fe feat: date picker in page cutting time

--- client/src/views/CuttingTimeView.vue
+++ client/src/views/CuttingTimeView.vue
@@ -1,36 +1,15 @@
 <script setup lang="ts">
-import { ref, shallowRef } from 'vue'
-import { DatePicker, Select } from 'primevue'
-import DataStatsOne from '@/components/DataStats/DataStatsOne.vue'
+import { ref, watchEffect } from 'vue'
+import { DatePicker, FloatLabel } from 'primevue'
 import ChartOne from '@/components/Charts/ChartOne.vue'
-import ChartThree from '@/components/Charts/ChartThree.vue'
-import ChartTwo from '@/components/Charts/ChartTwo.vue'
-import MapOne from '@/components/Maps/MapOne.vue'
-import TableOne from '@/components/Tables/TableOne.vue'
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import BreadcrumbDefault from '@/components/Breadcrumbs/BreadcrumbDefault.vue'
 
-const selectOptions = [
-  { label: 'Day', value: 'day' },
-  { label: 'Month', value: 'month' }
-]
-
-const type = shallowRef<'day' | 'month'>('day')
 // default value = now month
-const monthValue = ref<Date>()
-
-function getDaysInMonth(today: Date) {
-  const currentYear = today.getFullYear()
-  const currentMonth = today.getMonth()
-
-  return new Date(year, month + 1, 0).getDate()
-}
-
-const handleChangeMonth = (date: Date) => {
-  monthValue.value = date
-  console.log(new Date(monthValue.value))
-  console.log(monthValue.value, typeof monthValue.value)
-}
+const monthValue = ref<Date>(new Date())
+watchEffect(() => {
+  console.log('monthValue', monthValue.value)
+})
 </script>
 
 <template>
@@ -40,22 +19,18 @@ const handleChangeMonth = (date: Date) => {
       <DataStatsOne />
     </div> -->
     <div class="mb-3 justify-end gap-4 sm:flex">
-      <Select
-        :model-value="type"
-        @update:model-value="type = $event"
-        :options="selectOptions"
-        option-label="label"
-        option-value="value"
-        :default-value="type"
-      />
-      <DatePicker
-        v-if="type === 'month'"
-        view="month"
-        showIcon
-        @update:model-value="(date) => handleChangeMonth(date as Date)"
-        :default-value="monthValue"
-        date-format="mm/yy"
-      />
+      <FloatLabel>
+        <DatePicker
+          v-model:model-value="monthValue"
+          :default-value="monthValue"
+          inputId="over_label"
+          view="month"
+          showIcon
+          iconDisplay="input"
+          date-format="mm/yy"
+        />
+        <label v-if="!monthValue" for="over_label">Select Month</label>
+      </FloatLabel>
     </div>
 
     <div class="mt-4 grid grid-cols gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
