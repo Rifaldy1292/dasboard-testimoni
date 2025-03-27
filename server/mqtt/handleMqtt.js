403379cf87abeb090dee382ec7b4f80c69a8c6e3
jonfry1175 <jonfrymarbun@gmail.com>
2025-03-27 17:39:38 +0700

    node-red

--- server/mqtt/handleMqtt.js
+++ server/mqtt/handleMqtt.js
@@ -1,38 +1,55 @@
 /**
  * Handles MQTT connections and messages, updating machine status and logs, and
  * sending updates via WebSocket.
- * 
+ *
  * @param {mqtt.Client} mqttClient - The MQTT client instance.
  * @param {WebSocket.Server} wss - The WebSocket server instance.
  */
-const { Machine } = require('../models');
-const { handleChangeMachineStatus, createMachineAndLogFirstTime, updateLastMachineLog } = require('./MachineMqtt');
-const WebSocket = require('ws');
-require('../websocket/handleWebsocket');
+const { Machine } = require("../models");
+const {
+  handleChangeMachineStatus,
+  createMachineAndLogFirstTime,
+  updateLastMachineLog,
+} = require("./MachineMqtt");
+const WebSocket = require("ws");
+require("../websocket/handleWebsocket");
 
 const mqttTopics = [
-    'mc-1/data', 'mc-2/data', 'mc-3/data', 'mc-4/data', 'mc-5/data',
-    'mc-6/data', 'mc-7/data', 'mc-8/data', 'mc-9/data', 'mc-10/data',
-    'mc-11/data', 'mc-12/data', 'mc-13/data', 'mc-14/data', 'mc-15/data', 'mc-16/data'
+  "mc-1/data",
+  "mc-2/data",
+  "mc-3/data",
+  "mc-4/data",
+  "mc-5/data",
+  "mc-6/data",
+  "mc-7/data",
+  "mc-8/data",
+  "mc-9/data",
+  "mc-10/data",
+  "mc-11/data",
+  "mc-12/data",
+  "mc-13/data",
+  "mc-14/data",
+  "mc-15/data",
+  "mc-16/data",
 ];
 
 /**
- * 
- * @param {mqtt.Client} mqttClient 
- * @param {WebSocket.Server} wss 
+ *
+ * @param {mqtt.Client} mqttClient
+ * @param {WebSocket.Server} wss
  */
 const handleMqtt = (mqttClient, wss) => {
-    mqttClient.on('connect', async () => {
-        mqttTopics.forEach(topic => {
-            console.log('MQTT client connected', topic);
-            mqttClient.subscribe(topic);
-        });
+  mqttClient.on("connect", async () => {
+    mqttTopics.forEach((topic) => {
+      console.log("MQTT client connected", topic);
+      mqttClient.subscribe(topic);
     });
+  });
 
-    mqttClient.on('message', async (topic, message) => {
-        // console.time('Proses');
-        try {
-            /**
+  mqttClient.on("message", async (topic, message) => {
+    // console.time('Proses');
+    try {
+      /**
              * Parses the received MQTT message into a JavaScript object.
              * @type {Object}
              * @example 
@@ -48,47 +65,48 @@ const handleMqtt = (mqttClient, wss) => {
              *     "calculate_total_cutting_time": 1.2222
              }
              */
-            const parseMessage = JSON.parse(message.toString());
-            // console.log(parseMessage, 77777)
+      const parseMessage = JSON.parse(message.toString());
+      // console.log(parseMessage, 77777)
 
-            // TODO: ini bisa improve performa, include MachineLog, 
-            const existMachine = await Machine.findOne({ where: { name: parseMessage.name }, attributes: ['id', 'status'] });
+      // TODO: ini bisa improve performa, include MachineLog,
+      const existMachine = await Machine.findOne({
+        where: { name: parseMessage.name },
+        attributes: ["id", "status"],
+      });
 
-            // create machine & log if machine not exist
-            if (existMachine === null) {
-                return await createMachineAndLogFirstTime(parseMessage);
-            }
+      // create machine & log if machine not exist
+      if (existMachine === null) {
+        return await createMachineAndLogFirstTime(parseMessage);
+      }
 
-            // if status change
-            if (existMachine.status !== parseMessage.status) {
-                await handleChangeMachineStatus(existMachine, parseMessage, wss);
-            }
+      // if status change
+      if (existMachine.status !== parseMessage.status) {
+        await handleChangeMachineStatus(existMachine, parseMessage, wss);
+      }
 
-            await updateLastMachineLog(existMachine.id);
-            // await updateLastMachineLog(existMachine.id, runningHour);
+      await updateLastMachineLog(existMachine.id);
+      // await updateLastMachineLog(existMachine.id, runningHour);
+    } catch (error) {
+      if (error.message === "Unexpected token < in JSON at position 0") {
+        return console.error({ error, message: "Invalid JSON" });
+      }
+      console.error({ error, message: error.message });
+    }
 
-
-        } catch (error) {
-            if (error.message === 'Unexpected token < in JSON at position 0') {
-                return console.error({ error, message: 'Invalid JSON' });
-            }
-            console.error({ error, message: error.message });
-        }
-
-        // wss.clients.forEach(async (client) => {
-        //     if (client.readyState === WebSocket.OPEN) {
-        //         const percentageMessage = messageTypeWebsocketClient.get(client)?.has('percentage');
-        //         if (percentageMessage) {
-        //             console.log(true)
-        //             await MachineWebsocket.percentages(client);
-        //         }
-        //         // else {
-        //         //     // console.log(false)
-        //         // }
-        //     }
-        // });
-        // console.timeEnd('Proses');
-    });
-}
+    // wss.clients.forEach(async (client) => {
+    //     if (client.readyState === WebSocket.OPEN) {
+    //         const percentageMessage = messageTypeWebsocketClient.get(client)?.has('percentage');
+    //         if (percentageMessage) {
+    //             console.log(true)
+    //             await MachineWebsocket.percentages(client);
+    //         }
+    //         // else {
+    //         //     // console.log(false)
+    //         // }
+    //     }
+    // });
+    // console.timeEnd('Proses');
+  });
+};
 
 module.exports = handleMqtt;
