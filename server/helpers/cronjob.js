4eb3fa8bd9f44866bfc2c5004e6bfd19129e3374
jonfry1175 <jonfrymarbun@gmail.com>
2025-05-03 20:18:30 +0700

    backup db

--- server/helpers/cronjob.js
+++ server/helpers/cronjob.js
@@ -131,7 +131,7 @@ const deleteCncFiles = async () => {
 const handleCronJob = async () => {
   await createCuttingTime();
   await createDailyConfig();
-  // await handleResetMachineStatus();
+  await handleResetMachineStatus();
 
   const { startHour, startMinute } = config;
   cron.schedule(`${startMinute} ${startHour} * * *`, async () => {
