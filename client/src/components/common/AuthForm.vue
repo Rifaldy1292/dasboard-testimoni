b4d9c652d439a4d13060e6c29bbf4b817f95ebbc
jonfry1175 <jonfrymarbun@gmail.com>
2025-05-20 18:12:58 +0700

    fe fix: color dark label form

--- client/src/components/common/AuthForm.vue
+++ client/src/components/common/AuthForm.vue
@@ -119,7 +119,7 @@ const showFormField = computed<ShowFormField>(() => {
           name="NIK"
           :default-value="userData?.NIK"
           :useGrouping="false"
-          class="w-full text-gray-800 dark:text-white text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+          class="w-full dark:text-gray-800 text-white text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
           placeholder="Enter NIK"
         />
         <Message v-if="$form.NIK?.invalid" severity="error" size="small" variant="simple">{{
@@ -136,7 +136,7 @@ const showFormField = computed<ShowFormField>(() => {
               :default-value="userData?.name"
               type="text"
               required
-              class="w-full dark:text-white text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+              class="w-full text-white dark:text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
               placeholder="Enter name"
             />
             <svg
@@ -162,12 +162,12 @@ const showFormField = computed<ShowFormField>(() => {
       <FormField v-if="showFormField.password" name="password">
         <div>
           <label class="text-gray-800 dark:text-white text-sm mb-2 block">Password</label>
-          <div class="relative flex items-center">
+          <div class="relative flex items-center full text-white dark:text-gray-800">
             <InputText
               @update:model-value="password = $event"
               :type="showPassword ? 'text' : 'password'"
               required
-              class="w-full text-gray-800 dark:text-white text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+              class="w-full text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
               placeholder="Enter password"
             />
             <button
@@ -191,7 +191,7 @@ const showFormField = computed<ShowFormField>(() => {
             <InputText
               :type="showConfirmPassword ? 'text' : 'password'"
               required
-              class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+              class="w-full dark:text-gray-800 text-white text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
               placeholder="Enter confirm password"
             />
             <button
@@ -245,109 +245,4 @@ const showFormField = computed<ShowFormField>(() => {
       </p>
     </div>
   </Form>
-  <!-- component for page forgot password -->
-  <!-- <template v-if="isResetPasswordPage && userData">
-    <Form
-      v-slot="$form"
-      @submit="submit"
-      :resolver="resolver"
-      :validate-on-blur="true"
-      :validate-on-submit="true"
-      :validate-on-value-update="true"
-    >
-      <div class="mt-8 space-y-4">
-        <div>
-          <label class="text-gray-800 text-sm mb-2 block">NIK</label>
-          <InputNumber
-            name="NIK"
-            disabled
-            :default-value="userData?.NIK"
-            :useGrouping="false"
-            class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-            placeholder="Enter NIK"
-          />
-          <Message v-if="$form.NIK?.invalid" severity="error" size="small" variant="simple">{{
-            $form.NIK?.error?.message
-          }}</Message>
-        </div>
-
-        <FormField name="name">
-          <div>
-            <label class="text-gray-800 text-sm mb-2 block">Name</label>
-            <div class="relative flex items-center">
-              <InputText
-                :default-value="userData?.name"
-                type="text"
-                required
-                disabled
-                class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-                placeholder="Enter name"
-              />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#bbb"
-                stroke="#bbb"
-                class="w-4 h-4 absolute right-4"
-                viewBox="0 0 24 24"
-              >
-                <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
-                <path
-                  d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
-                  data-original="#000000"
-                ></path>
-              </svg>
-            </div>
-            <Message v-if="$form.name?.invalid" severity="error" size="small" variant="simple">{{
-              $form.name?.error?.message
-            }}</Message>
-          </div>
-        </FormField>
-
-        <FormField name="password">
-          <div>
-            <label class="text-gray-800 text-sm mb-2 block">Password</label>
-            <div class="relative flex items-center">
-              <InputText
-                :type="showPassword ? 'text' : 'password'"
-                required
-                class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-                placeholder="Enter password"
-              />
-              <button
-                type="button"
-                @click="showPassword = !showPassword"
-                class="w-4 h-4 absolute right-4"
-              >
-                <i :class="showPassword ? 'fa fa-eye-slash' : 'fa fa-eye'" />
-              </button>
-            </div>
-            <Message
-              v-if="$form.password?.invalid"
-              severity="error"
-              size="small"
-              variant="simple"
-              >{{ $form.password?.error?.message }}</Message
-            >
-          </div>
-        </FormField>
-
-        <div class="!mt-8">
-          <button
-            type="submit"
-            class="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
-          >
-            Reset Password
-          </button>
-        </div>
-        <p class="text-gray-800 text-sm !mt-8 text-center">
-          Already have an account?
-          <a
-            href="/login"
-            class="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold"
-            >Go back to login</a
-          >
-        </p>
-      </div>
-    </Form>
-  </template> -->
 </template>
