76b0d265d5adba3fe7dc1bb493af586c2ec7487d
jonfry1175 <jonfrymarbun@gmail.com>
2025-07-01 16:03:56 +0700

    fe feat(RemoveFile): enable conditional removal for specific machine types

--- client/src/components/modules/TransferFile/RemoveFile.vue
+++ client/src/components/modules/TransferFile/RemoveFile.vue
@@ -135,9 +135,9 @@ const handleClickButton = (button: 'removeAll' | 'remove' | 'undo', fileName?: s
 }
 
 const isDisableRemoveAll = computed<boolean>(() => {
-  return false
-  // const name = selectedOneMachine.value?.name as MachineName
+  const name = selectedOneMachine.value?.name as MachineName
   // const disabled = name !== 'MC-16' && name !== 'MC-6'
+  return name !== 'MC-3'
   // return disabled
 })
 </script>
