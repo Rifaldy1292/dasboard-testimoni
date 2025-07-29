1cc0e2865a74deed8220670b376e4b062e12f295
jonfry1175 <jonfrymarbun@gmail.com>
2025-07-29 13:53:06 +0700

    feat(TransferFile): improve file upload section with machine selection validation and enhanced button handling

--- client/src/components/modules/TransferFile/TransferFile.vue
+++ client/src/components/modules/TransferFile/TransferFile.vue
@@ -235,114 +235,129 @@ const handleChange = async (event: Event) => {
       <!-- <div class="col-span-5 xl:col-span-3"> -->
       <!-- Machine Section -->
       <AdditionalFields :is-disable-all="isCreatedMainProgram" />
-      <template v-if="selectedAction === 'Upload File'">
-        <template v-if="!isCreatedMainProgram">
-          <!-- upload folder -->
-          <div class="flex items-center">
-            <div class="h-10">
-              <span class="font-medium text-black dark:text-white"
-                >Upload your {{ uploadType }}</span
-              >
-            </div>
-          </div>
 
-          <!-- File Upload Section -->
-          <div
-            id="FileUpload"
-            v-tooltip.top="!selectedOneMachine ? 'Please select machine first' : ''"
-            :class="`relative mb-2  block w-full h-60 cursor-pointer appearance-none rounded border-2 border-dashed border-primary bg-gray py-4 px-4 dark:bg-meta-4 sm:py-7.5 ${
-              disabled.disableUpload ? 'cursor-not-allowed opacity-50' : ''
-            }`"
+      <!-- only show if select machine -->
+
+      <template v-if="!selectedOneMachine">
+        <!-- text red -->
+        <div class="flex items-center justify-center">
+          <span class="text-xl font-medium text-red-500 dark:text-red-400"
+            >Please select machine first</span
           >
-            <FormField name="files">
-              <input
-                type="file"
-                :disabled="disabled.disableUpload"
-                @change="handleChange"
-                :webkitdirectory="uploadType === 'folder'"
-                multiple
-                :style="{
-                  cursor: disabled.disableUpload ? 'not-allowed' : 'pointer'
-                }"
-                class="absolute inset-0 z-50 m-0 h-full w-full cursor-pointer p-0 opacity-0 outline-none"
-              />
-            </FormField>
-            <div class="flex flex-col items-center justify-center space-y-3">
-              <span
-                class="flex h-10 w-10 items-center justify-center rounded-full border border-stroke bg-white dark:border-strokedark dark:bg-boxdark"
-              >
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M1.99967 9.33337C2.36786 9.33337 2.66634 9.63185 2.66634 10V12.6667C2.66634 12.8435 2.73658 13.0131 2.8616 13.1381C2.98663 13.2631 3.1562 13.3334 3.33301 13.3334H12.6663C12.8431 13.3334 13.0127 13.2631 13.1377 13.1381C13.2628 13.0131 13.333 12.8435 13.333 12.6667V10C13.333 9.63185 13.6315 9.33337 13.9997 9.33337C14.3679 9.33337 14.6663 9.63185 14.6663 10V12.6667C14.6663 13.1971 14.4556 13.7058 14.0806 14.0809C13.7055 14.456 13.1968 14.6667 12.6663 14.6667H3.33301C2.80257 14.6667 2.29387 14.456 1.91879 14.0809C1.54372 13.7058 1.33301 13.1971 1.33301 12.6667V10C1.33301 9.63185 1.63148 9.33337 1.99967 9.33337Z"
-                    fill="#3C50E0"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M7.5286 1.52864C7.78894 1.26829 8.21106 1.26829 8.4714 1.52864L11.8047 4.86197C12.0651 5.12232 12.0651 5.54443 11.8047 5.80478C11.5444 6.06513 11.1223 6.06513 10.8619 5.80478L8 2.94285L5.13807 5.80478C4.87772 6.06513 4.45561 6.06513 4.19526 5.80478C3.93491 5.54443 3.93491 5.12232 4.19526 4.86197L7.5286 1.52864Z"
-                    fill="#3C50E0"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M7.99967 1.33337C8.36786 1.33337 8.66634 1.63185 8.66634 2.00004V10C8.66634 10.3682 8.36786 10.6667 7.99967 10.6667C7.63148 10.6667 7.33301 10.3682 7.33301 10V2.00004C7.33301 1.63185 7.63148 1.33337 7.99967 1.33337Z"
-                    fill="#3C50E0"
-                  />
-                </svg>
-              </span>
-              <p class="text-sm font-medium">
-                <span class="text-primary">Click to upload</span> or drag and drop
-              </p>
-              <!-- <p class="mt-1.5 text-sm font-medium">SVG, PNG, JPG or GIF</p> -->
-              <!-- <p class="text-sm font-medium">(max 3MB)</p> -->
-            </div>
-          </div>
-        </template>
-        <!-- Save  Buttons -->
-        <div class="flex justify-end gap-4.5">
-          <button
-            class="flex justify-center rounded border border-stroke py-2 px-6 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
-            type="button"
-            @click="handleClearFile"
-          >
-            Clear File
-          </button>
-          <button
-            v-if="isCreatedMainProgram"
-            :disabled="disabled.disableExecute"
-            class="flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:bg-opacity-90"
-            type="submit"
-            @click="handleSubmit"
-          >
-            Transfer Files
-          </button>
-          <button
-            v-tooltip.top="!inputFiles.length ? 'Please upload file first' : ''"
-            v-if="!isCreatedMainProgram"
-            :disabled="disabled.disableExecute"
-            @click="handleExecute"
-            :style="{
-              cursor: disabled.disableExecute ? 'not-allowed' : 'pointer'
-            }"
-            class="flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:bg-opacity-90"
-            type="submit"
-          >
-            {{ selectedZooler ? 'Transfer File' : 'Execute' }}
-          </button>
         </div>
       </template>
-      <template v-else-if="selectedAction === 'Remove File'">
-        <RemoveFile />
+
+      <template v-else>
+        <template v-if="selectedAction === 'Upload File'">
+          <template v-if="!isCreatedMainProgram">
+            <!-- upload folder -->
+            <div class="flex items-center">
+              <div class="h-10">
+                <span class="font-medium text-black dark:text-white"
+                  >Upload your {{ uploadType }}</span
+                >
+              </div>
+            </div>
+
+            <!-- File Upload Section -->
+            <div
+              id="FileUpload"
+              v-tooltip.top="!selectedOneMachine ? 'Please select machine first' : ''"
+              :class="`relative mb-2  block w-full h-60 cursor-pointer appearance-none rounded border-2 border-dashed border-primary bg-gray py-4 px-4 dark:bg-meta-4 sm:py-7.5 ${
+                disabled.disableUpload ? 'cursor-not-allowed opacity-50' : ''
+              }`"
+            >
+              <FormField name="files">
+                <input
+                  type="file"
+                  :disabled="disabled.disableUpload"
+                  @change="handleChange"
+                  :webkitdirectory="uploadType === 'folder'"
+                  multiple
+                  :style="{
+                    cursor: disabled.disableUpload ? 'not-allowed' : 'pointer'
+                  }"
+                  class="absolute inset-0 z-50 m-0 h-full w-full cursor-pointer p-0 opacity-0 outline-none"
+                />
+              </FormField>
+              <div class="flex flex-col items-center justify-center space-y-3">
+                <span
+                  class="flex h-10 w-10 items-center justify-center rounded-full border border-stroke bg-white dark:border-strokedark dark:bg-boxdark"
+                >
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M1.99967 9.33337C2.36786 9.33337 2.66634 9.63185 2.66634 10V12.6667C2.66634 12.8435 2.73658 13.0131 2.8616 13.1381C2.98663 13.2631 3.1562 13.3334 3.33301 13.3334H12.6663C12.8431 13.3334 13.0127 13.2631 13.1377 13.1381C13.2628 13.0131 13.333 12.8435 13.333 12.6667V10C13.333 9.63185 13.6315 9.33337 13.9997 9.33337C14.3679 9.33337 14.6663 9.63185 14.6663 10V12.6667C14.6663 13.1971 14.4556 13.7058 14.0806 14.0809C13.7055 14.456 13.1968 14.6667 12.6663 14.6667H3.33301C2.80257 14.6667 2.29387 14.456 1.91879 14.0809C1.54372 13.7058 1.33301 13.1971 1.33301 12.6667V10C1.33301 9.63185 1.63148 9.33337 1.99967 9.33337Z"
+                      fill="#3C50E0"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M7.5286 1.52864C7.78894 1.26829 8.21106 1.26829 8.4714 1.52864L11.8047 4.86197C12.0651 5.12232 12.0651 5.54443 11.8047 5.80478C11.5444 6.06513 11.1223 6.06513 10.8619 5.80478L8 2.94285L5.13807 5.80478C4.87772 6.06513 4.45561 6.06513 4.19526 5.80478C3.93491 5.54443 3.93491 5.12232 4.19526 4.86197L7.5286 1.52864Z"
+                      fill="#3C50E0"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M7.99967 1.33337C8.36786 1.33337 8.66634 1.63185 8.66634 2.00004V10C8.66634 10.3682 8.36786 10.6667 7.99967 10.6667C7.63148 10.6667 7.33301 10.3682 7.33301 10V2.00004C7.33301 1.63185 7.63148 1.33337 7.99967 1.33337Z"
+                      fill="#3C50E0"
+                    />
+                  </svg>
+                </span>
+                <p class="text-sm font-medium">
+                  <span class="text-primary">Click to upload</span> or drag and drop
+                </p>
+                <!-- <p class="mt-1.5 text-sm font-medium">SVG, PNG, JPG or GIF</p> -->
+                <!-- <p class="text-sm font-medium">(max 3MB)</p> -->
+              </div>
+            </div>
+          </template>
+          <!-- Save  Buttons -->
+          <div class="flex justify-end gap-4.5">
+            <button
+              class="flex justify-center rounded border border-stroke py-2 px-6 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
+              type="button"
+              @click="handleClearFile"
+            >
+              Clear File
+            </button>
+            <button
+              v-if="isCreatedMainProgram"
+              :disabled="disabled.disableExecute"
+              class="flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:bg-opacity-90"
+              type="submit"
+              @click="handleSubmit"
+            >
+              Transfer Files
+            </button>
+            <button
+              v-tooltip.top="!inputFiles.length ? 'Please upload file first' : ''"
+              v-if="!isCreatedMainProgram"
+              :disabled="disabled.disableExecute"
+              @click="handleExecute"
+              :style="{
+                cursor: disabled.disableExecute ? 'not-allowed' : 'pointer'
+              }"
+              class="flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:bg-opacity-90"
+              type="submit"
+            >
+              {{ selectedZooler ? 'Transfer File' : 'Execute' }}
+            </button>
+          </div>
+        </template>
+        <template v-else-if="selectedAction === 'Remove File'">
+          <RemoveFile />
+        </template>
       </template>
     </div>
+
     <template v-if="!isCreatedMainProgram">
       <template v-for="(file, index) in inputFiles" :key="file.name">
         <PreviewFile :file :index />
